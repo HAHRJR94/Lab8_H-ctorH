6e96ae897ea799c7e433b87107fdad519900e751
HAHRJR94 <hectorhenriquez9@gmail.com>
2019-09-07 17:11:35 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import _ from "lodash";
+import ItemComponent from './components/ItemComponent';
+import ButtonComponent from './components/ButtonComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      data:[
+        {
+          image: 'Hamburger.png',
+          name: 'Hamburger',
+          portion: '500g',
+          price: 40
+        }
+      ]
+    }
+  }
+
+  _remove(position){
+    let { data } = this.state;
+
+    let newData = [
+      ...data.slice(0, position),
+      ...data.slice(position + 1),
+    ]
+
+
+    this.setState({data: newData});
+  }
+
+  _add(){
+    let { data } = this.state;
+
+    let newData = [
+      ...data,
+      {
+        image: 'papas.png',
+        name: 'Potatoes',
+        portion: '140g',
+        price: Math.floor(Math.random() * 20)
+      }
+    ]
+
+    this.setState({ data: newData });
+  }
+
+  _getTotal(){
+    return _.sumBy(this.state.data, (o) => {return o.price});
+  }
+   
+  
+  render() {
+    return (
+      <div className="app">
+        <h1>Lists example</h1>
+        <ul className="todo-list">
+          {this.state.data.map(
+              (item, index) =>
+                <ItemComponent data={item} key={index} onRemove={ () => this._remove(index)} />              
+            )
+          }
+        </ul>
+        <div className="footer">
+            <ButtonComponent 
+              onClick={this._add.bind(this)}
+              name="Add Product"
+            />
+            <h4>
+              ${this._getTotal()}
+            </h4>
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
